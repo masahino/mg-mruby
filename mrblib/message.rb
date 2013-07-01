9c8820265ea2a20479655158814e6016c0e56c98
masahino <masahino@gmail.com>
2013-07-01 22:52:46 +0900

    indent

--- mrblib/message.rb
+++ mrblib/message.rb
@@ -1,8 +1,13 @@
 def MG.message(message_str)
-	cur_buf = MG::Buffer.current()
-	MG.switch_to_buffer("*Messages*")
-        MG.end_of_buffer()
-	MG.insert(message_str)
-	MG.newline()
-	MG.switch_to_buffer(cur_buf.name)
+  cur_buf = MG::Buffer.current()
+  ret = MG.switch_to_buffer("*Messages*")
+  if MG::Buffer.current.name != "*Messages*"
+    MG.debug_log "failed to change buffer "
+  end
+  MG.end_of_buffer()
+  MG.insert(message_str)
+  MG.newline()
+  MG.switch_to_buffer(cur_buf.name)
 end
+
+MG.message "load message.rb done."
